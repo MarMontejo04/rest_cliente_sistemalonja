78cfe6ee7821676e56135a8ef1c919b47a01a778
MarMontejo04 <mariana.montejo04@gmail.com>
2025-12-02 02:07:43 -0600

    Axios render

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
         const clienteAxios=axios.create({
-            baseURL:"http://localhost:3001"
+            baseURL:"https://rest-mongodb-18rp.onrender.com/"
         })
 
         export default clienteAxios
